13ed8d28cd321c9ffe1d6df3bcd3bb0348b3dc03
iteamstag <info.iteamstag@gmail.com>
2022-12-26 01:58:37 +0200

    test

--- MyApp/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
-
-import { AppModule } from './app/app.module';
-
-runNativeScriptAngularApp({
-  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
-});
-
